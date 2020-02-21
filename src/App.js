1d7aad2f34bd1762d124ac3fc136b99942ae4cbd
Yevhenii <e.osadchuy@gmail.com>
2020-02-21 11:38:53 +0200

    First upload

--- src/App.js
+++ src/App.js
@@ -6,19 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Header
       </header>
+      <iframe src="https://api.monobank.ua/" width={500} height={500}>
+        some text
+      </iframe>
     </div>
   );
 }
